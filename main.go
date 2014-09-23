eb94a3619ee5074ff77c5bac6bf107f521e8c97f
kiyor <cyfkiyor@gmail.com>
2014-09-23 01:40:18 +0000

    add new func

--- main.go
+++ main.go
@@ -6,7 +6,7 @@
 
 * Creation Date : 09-16-2014
 
-* Last Modified : Thu 18 Sep 2014 09:04:45 PM UTC
+* Last Modified : Tue 23 Sep 2014 01:08:08 AM UTC
 
 * Created By : Kiyor
 
@@ -15,16 +15,14 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/miekg/unbound"
 	"github.com/wsxiaoys/terminal/color"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -41,19 +39,30 @@ type reslov struct {
 
 var (
 	domain   string
+	url      string
+	doChk    bool
 	reDomain = regexp.MustCompile(`([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})`)
 )
 
 func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	if len(os.Args) == 1 {
 		fmt.Println("use digger domain.com")
 		os.Exit(1)
+	}
+	if len(os.Args) == 2 {
+		doChk = false
+	}
+	if len(os.Args) == 3 {
+		doChk = true
+		url = os.Args[2]
 	}
 	domain = os.Args[1]
 	if reDomain.MatchString(domain) {
 		val := reDomain.FindStringSubmatch(domain)
 		domain = val[0]
 	}
+	checkres = make(map[string]check)
 }
 
 func main() {
@@ -99,12 +108,36 @@ func (r *reslov) digger(domain string) {
 	var wg sync.WaitGroup
 	for _, a1 := range a {
 		wg.Add(1)
-		go func(a1 string) {
-			loc := ip2loc(a1)
-			r.res += color.Sprintf("@{c}A@{|}     %-18s %s %s\n", a1, loc.CountryCode, loc.City)
+		go func(a1 string, r *reslov) {
+			var w sync.WaitGroup
+			var loc ipLoc
+			w.Add(1)
+			go func(a1 string, loc *ipLoc) {
+				loc.ip2loc(a1)
+				w.Done()
+			}(a1, &loc)
+			if doChk {
+				w.Add(1)
+				go func(a1 string) {
+					r.headerCheck(url, a1)
+					w.Done()
+				}(a1)
+			}
+			w.Wait()
+			if doChk {
+				if checkres[a1].code < 500 {
+					r.res += color.Sprintf("@{c}A@{|}     %-18s %s %s @{g}[%d]@{|} %v\n", a1, loc.CountryCode, loc.City, checkres[a1].code, checkres[a1].header)
+				} else {
+					r.res += color.Sprintf("@{c}A@{|}     %-18s %s %s @{r}[%d]@{|} %v\n", a1, loc.CountryCode, loc.City, checkres[a1].code, checkres[a1].header)
+				}
+			} else {
+				r.res += color.Sprintf("@{c}A@{|}     %-18s %s %s\n", a1, loc.CountryCode, loc.City)
+			}
+			// 		r.writeHost("cctest1.youbibi.com", a1)
 			wg.Done()
-		}(a1)
+		}(a1, r)
 	}
+	// 	fmt.Println(checkres)
 	wg.Wait()
 }
 
@@ -126,22 +159,5 @@ func getReslovs(dir string) (rs []reslov) {
 		rs = append(rs, r)
 		return nil
 	})
-	return
-}
-
-type ipLoc struct {
-	CountryCode string `json:"country_code"`
-	CountryName string `json:"country_name"`
-	RegionName  string `json:"region_name"`
-	City        string `json:"city"`
-}
-
-func ip2loc(ip string) (loc ipLoc) {
-	res, err := http.Get(fmt.Sprintf("http://freegeoip.net/json/%s", ip))
-	if err != nil {
-		log.Fatalf("error %s\n", err.Error())
-	}
-	b, _ := ioutil.ReadAll(res.Body)
-	json.Unmarshal(b, &loc)
 	return
 }
